0e28e271ef8379a598bdef8206987ffb88b0c4a4
Adrien Nader <adrien@notk.org>
2012-10-13 18:53:19 +0200

    get-all-prebuilt-binaries: add a few echos to indicate steps.

--- get-all-prebuilt-binaries-i686.sh
+++ get-all-prebuilt-binaries-i686.sh
@@ -2,13 +2,20 @@
 
 for d in cross_toolchain_32 windows_32; do
   export YYPREFIX="/opt/${d}"
+
+  echo "Initializing yypkg in ${YYPREFIX}."
   yypkg -init
+
   sed -i "s/SERIES/${d}/" "${YYPREFIX}/etc/yypkg.d/sherpa.conf"
+
+  echo "Setting up predicates for ${d}."
   if [ "${d}" = "cross_toolchain_32" ]; then
     yypkg -config -setpreds host="x86_64-slackware-linux"
     yypkg -config -setpreds target="i686-w64-mingw32"
   else if [ "${d}" = "windows_32" ]; then
     yypkg -config -setpreds host="i686-w64-mingw32"
   fi; fi
+
+  echo "Downloading and installing packages for ${d}."
   sherpa -install '*'
 done
