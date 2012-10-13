a2e700094b0eb479f813ba19f5ee0a17e52cc895
Adrien Nader <adrien@notk.org>
2012-10-13 18:48:19 +0200

    get-all-prebuilt-binaries: fix missing YYPREFIX.

--- get-all-prebuilt-binaries-i686.sh
+++ get-all-prebuilt-binaries-i686.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -eu
 
 for d in cross_toolchain_32 windows_32; do
-  yypkg -prefix /opt/${d} -init
-  sed -i "s/SERIES/${d}/" /opt/${d}/etc/yypkg.d/sherpa.conf
+  export YYPREFIX="/opt/${d}"
+  yypkg -init
+  sed -i "s/SERIES/${d}/" "${YYPREFIX}/etc/yypkg.d/sherpa.conf"
   if [ "${d}" = "cross_toolchain_32" ]; then
     yypkg -config -setpreds host="x86_64-slackware-linux"
     yypkg -config -setpreds target="i686-w64-mingw32"
   else if [ "${d}" = "windows_32" ]; then
     yypkg -config -setpreds host="i686-w64-mingw32"
   fi; fi
-  sherpa -prefix /opt/${d} -install '*'
+  sherpa -install '*'
 done
