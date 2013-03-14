f37cd5494aab2806a2cab3dac8870815539793e4
Adrien Nader <adrien@notk.org>
2013-03-14 09:59:31 +0100

    get-all-prebuilt-binaries: add missing "native_toolchain" repo.

--- get-all-prebuilt-binaries-i686.sh
+++ get-all-prebuilt-binaries-i686.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-for d in cross_toolchain_32 windows_32; do
+for d in native_toolchain cross_toolchain_32 windows_32; do
   export YYPREFIX="/opt/${d}"
 
   echo "Initializing yypkg in ${YYPREFIX}."
@@ -12,9 +12,11 @@ for d in cross_toolchain_32 windows_32; do
   if [ "${d}" = "cross_toolchain_32" ]; then
     yypkg -config -setpreds host="x86_64-slackware-linux"
     yypkg -config -setpreds target="i686-w64-mingw32"
-  else if [ "${d}" = "windows_32" ]; then
+  elif [ "${d}" = "windows_32" ]; then
     yypkg -config -setpreds host="i686-w64-mingw32"
-  fi; fi
+  elif [ "${d}" = "native_toolchain" ]; then
+    yypkg -config -setpreds host="x86_64-slackware-linux"
+  fi
 
   echo "Downloading and installing packages for ${d}."
   sherpa -install 'all'
