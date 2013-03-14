f61108d22efb4f3ce9ac127f1dfef49249075afe
Adrien Nader <adrien@notk.org>
2013-03-14 21:07:55 +0100

    release: better rsync flags (-avzP).

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ SHERPA_GEN="${CWD}/yypkg/src/sherpa_gen.native"
 
 mkdir -p "${LOCATION}/repositories"
 
-rsync --progress "${LOCATION}/system.tar.xz" "${MIRROR}"
+rsync -avzP "${LOCATION}/system.tar.xz" "${MIRROR}/"
 
 for REPO in $(find "${LOCATION}/packages" -mindepth 1 -maxdepth 1 -type d); do
   echo "Setting up ${REPO}"
@@ -18,6 +18,6 @@ for REPO in $(find "${LOCATION}/packages" -mindepth 1 -maxdepth 1 -type d); do
   OUTPUT="${LOCATION}/repositories/${REPO_NAME}"
   mkdir -p "${OUTPUT}"
   "${SHERPA_GEN}" "${REPO}" "${OUTPUT}"
-  rsync --archive --progress "${REPO}" "${MIRROR}"
-  rsync --archive --exclude='memo_*' "${OUTPUT}/" "${MIRROR}/${REPO_NAME}/"
+  rsync -avzP "${REPO}" "${MIRROR}/"
+  rsync -avzP --exclude='memo_*' "${OUTPUT}/" "${MIRROR}/${REPO_NAME}/"
 done
