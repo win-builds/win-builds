5f2b74a9962b152b73b079db27162ec22906f07e
Adrien Nader <adrien@notk.org>
2013-03-19 23:07:29 +0100

    build-packages: build lua for edje.

--- build-packages.sh
+++ build-packages.sh
@@ -98,6 +98,7 @@ if echo "${KIND}" | grep -q ${NATIVE_TOOLCHAIN}; then
   start_build_daemon "${NATIVE_TOOLCHAIN}"
   queue_cond ${SBo}/ocaml ""
   queue_cond ${SBo}/ocaml-findlib ""
+  queue_cond ${SBo}/lua ""
   for efl_lib in eina eet evas ecore embryo edje; do
     queue_cond ${SBo}/${efl_lib} ""
   done
