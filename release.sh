62de86476493f798d4efeaa9b97ce9efdc02f509
Adrien Nader <adrien@notk.org>
2013-03-19 21:53:41 +0100

    release: also copy logs.

--- release.sh
+++ release.sh
@@ -13,12 +13,15 @@ if [ -d "${DEST}" ]; then
   exit 1
 fi
 
-mkdir "${DEST}" "${DEST}/packages"
+mkdir "${DEST}" "${DEST}/packages" "${DEST}/logs"
 
 cp "${LOCATION}/system.tar.xz" "${DEST}"
 
-for REPO in $(find "${LOCATION}/packages" -mindepth 1 -maxdepth 1 -type d -printf '%P\n'); do
-  echo "Setting up ${REPO}"
-  cp -a "${LOCATION}/packages/${REPO}" "${DEST}/packages/${REPO}"
-  "${SHERPA_GEN}" "${DEST}/packages/${REPO}"
-done
+find "${LOCATION}/packages" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' \
+  | while read REPO; do
+      echo "Setting up ${REPO}."
+      for d in logs packages; do 
+        cp -a "${LOCATION}/${d}/${REPO}" "${DEST}/${d}/${REPO}"
+      done
+      "${SHERPA_GEN}" "${DEST}/packages/${REPO}"
+    done
