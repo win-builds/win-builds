a3b6ccf461d98f27ee53a0ef1a15d9c70b5916ed
Adrien Nader <adrien@notk.org>
2013-12-10 23:58:50 +0100

    web: add a "contact" page (IRC, bug tracker, mailing-list, email).

---
+++ web/contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include 'head.html'; ?>
+</head>
+
+<body>
+<?php include 'header.html'; ?>
+
+<h1>Contact</h1>
+
+<h2>Bug Tracker</h2>
+For most issues, you can use the <a href="bugs">bug tracker</a>.
+
+<h2>IRC</h2>
+You can also join the #mingw-w64 channel on OFTC (or on freenode where messages 
+will be relayed to OFTC).
+
+<h2>Mailing-list</h2>
+If you prefer, you can also send an email to the <a href="https://lists.sourceforge.net/lists/listinfo/mingw-w64-public">mingw-w64 mailing-list</a>.
+
+<h2>Mail</h2>
+If really none of the above fits your situation, you can send an e-mail to adrien AT notk.org.
+
+<?php include 'footer.html'; ?>
+</body>
+</html>
