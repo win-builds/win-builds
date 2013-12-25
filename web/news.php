0f592d430488e052812429ea7e20f67a3f884d5d
Adrien Nader <adrien@notk.org>
2013-12-25 22:16:02 +0100

    web/news.php: update for 1.3-rc1.

--- web/news.php
+++ web/news.php
@@ -9,12 +9,25 @@
 
 <h1>News</h1>
 
-<h3><a href="1.3-beta3">1.3-beta1 - 27/11/2013</a></h3>
-  Corrections for dbus and yypkg.exe which was an old binary in 1.2, improved
+<h3><a href="1.3-rc1">1.3-rc1 - 22/12/2013</a></h3>
+  Fixed the "mingw" symlink in the root dir on windows.<br>
+  Implement in C the logic to create and delete reparse points for the symlink
+  fallback instead of calling mklink.exe (which is not available on XP).<br>
+  Fixed wrong mirror in the Linux chroot.<br>
+  Fix extra symlinks to gfortran binaries.<br>
+  Restore .exe extensions for GCC files.<br>
+  Some files weren't removed when uninstalling a package on Windows.<br>
+  Update libjpeg (fixes
+<a href="http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-6629">CVE-2013-6629</a>).<br>
+  The OCaml cross-compiler was unable to build bindings to C libraries.<br>
+  Check that the 'win-builds-switch.sh' script is called in the right way by the user.
+
+<h3><a href="1.3-beta3">1.3-beta3 - 11/12/2013</a></h3>
+  Fixes for dbus and yypkg.exe which was an old binary in 1.2, improved
   documentation, documentation for using with MSYS. The package metadata format
   changes (the version field is now a free-form string).
 
-<h3><a href="1.3-beta2">1.3-beta1 - 27/11/2013</a></h3>
+<h3><a href="1.3-beta2">1.3-beta2 - 4/12/2013</a></h3>
   Several fixes and mostly documentation or usability improvements.
 
 <h3><a href="1.3-beta1">1.3-beta1 - 27/11/2013</a></h3>
