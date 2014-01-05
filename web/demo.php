a52afbfbac593c200351f8a7abc90bee264709ce
Adrien Nader <adrien@notk.org>
2014-01-05 22:01:00 +0100

    web/demo: update to the new video which features install scripts.

--- web/demo.php
+++ web/demo.php
@@ -3,55 +3,41 @@
 <head>
 <?php include 'head.html'; ?>
 <script>
-  function playback_rate(rate) {
-    var video = document.getElementById('video');
-    video.playbackRate = rate;
-  }
+  var data = [
+    { start: 0,   id: 'caption-0'   },
+    { start: 7,   id: 'caption-7'   },
+    { start: 15,  id: 'caption-15'  },
+    { start: 21,  id: 'caption-21'  },
+    { start: 24,  id: 'caption-24'  },
+    { start: 29,  id: 'caption-29'  },
+    { start: 39,  id: 'caption-39'  },
+    { start: 54,  id: 'caption-54'  },
+    { start: 65,  id: 'caption-65'  },
+    { start: 87,  id: 'caption-87'  },
+    { start: 117, id: 'caption-117' },
+    { start: 128, id: 'caption-128' }
+  ];
+
+  data.reverse();
+
   function hide_all() {
     var children = document.getElementById('captions').children;
     for (var i = 0; i < children.length; i++) {
       children[i].style.display = 'none';
     }
   }
-  function reset_all() {
-    hide_all();
-    playback_rate(1);
-  }
-  function show(id) {
-    var e = document.getElementById(id);
-    if (e != null) {
-      e.style.display = '';
-    }
-  }
-  var data = [
-    { start: 9,   cb: function() { reset_all(); show('caption-9')  } },
-    { start: 16,  cb: function() { reset_all(); show('caption-16') } },
-    { start: 21,  cb: function() { reset_all(); show('caption-21') } },
-    { start: 36,  cb: function() { reset_all(); show('caption-36') } },
-    { start: 41,  cb: function() { reset_all(); show('caption-41') } },
-    { start: 44,  cb: function() { reset_all(); show('caption-44') } },
-    { start: 52,  cb: function() { reset_all(); show('caption-52') } },
-    { start: 74,  cb: function() { reset_all(); show('caption-74') } },
-    { start: 101, cb: function() { reset_all(); show('caption-101') } },
-    { start: 107, cb: function() { reset_all(); show('caption-107') } },
-    { start: 116, cb: function() { reset_all(); show('caption-116') } },
-    { start: 135, cb: function() { reset_all(); show('caption-135') } },
-    { start: 140, cb: function() { reset_all(); show('caption-140'); playback_rate(4) } },
-    { start: 160, cb: function() { hide_all() } },
-    { start: 203, cb: function() { reset_all(); show('caption-203') } },
-    { start: 210, cb: function() { reset_all(); show('caption-210') } },
-    { start: 220, cb: function() { reset_all(); show('caption-220') } },
-    { start: 233, cb: function() { reset_all(); show('caption-233') } },
-    { start: 240, cb: function() { reset_all(); show('caption-240') } },
-    { start: 245, cb: function() { reset_all(); show('caption-245') } },
-    { start: 262, cb: function() { reset_all(); show('caption-262') } }
-  ];
-  data.reverse();
+
   function ontimeupdate_cb() {
     var video = document.getElementById('video');
     data.some(function(e, _i, _a) {
       if (e.start <= video.currentTime) {
-        e.cb();
+        hide_all();
+
+        var e = document.getElementById(e.id);
+        if (e != null) {
+          e.style.display = '';
+        }
+
         return true;
       }
       else {
@@ -68,70 +54,48 @@
 <h1>Demo of the installation on Windows 2012</h1>
 
 <video id="video" controls width="1024" height="768" ontimeupdate="ontimeupdate_cb()">
-  <source src="win-builds-demo.webm#t=9" type="video/webm" />
-  <source src="win-builds-demo.mp4#t=9" type="video/mp4" />
-  Your browser does not support HTML5 video.
+  <source src="win-builds-demo.webm#t=0" type="video/webm" />
+  <source src="win-builds-demo.mp4#t=0" type="video/mp4" />
+  Your browser does not support HTML5 video or doesn't handle either webm/vpx or mp4/h264.
 </video>
 <div id="captions">
-  <div id="caption-9" class="caption">
+  <div id="caption-0" class="caption">
     First thing is to <a href=http://win-builds.org/@@VERSION@@>download the bootstrap binaries</a>.
   </div>
-  <div id="caption-16" class="caption">
+  <div id="caption-7" class="caption">
     This puts us on the documentation page which covers most scenarios and provides the detailled installation instructions.
   </div>
-  <div id="caption-21" class="caption">
+  <div id="caption-15" class="caption">
     The bootstrap binaries are packaged in a zip file for now (installer has not been rebuilt yet) and we will simply extract them on the desktop.
   </div>
-  <div id="caption-36" class="caption">
-    We then need a command prompt (until the installer is available).
+  <div id="caption-21" class="caption">
+    For an installation outside of MSYS*/Cygwin, we simply run 'win-install.bat'.
   </div>
-  <div id="caption-41" class="caption">
-    Now, let's enter the directory that we've just extracted.
+  <div id="caption-24" class="caption">
+    It copies a few files, setups the installation directory and starts downloading the packages.<br>
+    There is roughly 60MB of packages to download; it's usually one or two minutes of download (above is using a gigabit link).
   </div>
-  <div id="caption-44" class="caption">
-    The YYPREFIX environment variable tells the 'yypkg' package manager which directory to install in.
+  <div id="caption-29" class="caption">
+    Installation is fairly quick. Note that symlink-related messages are harmless.
   </div>
-  <div id="caption-52" class="caption">
-    'yypkg -init' will create directories and configuration files with default values.
+  <div id="caption-39" class="caption">
+    After packages have been installed, the script will update fontconfig, pango, gdk and gtk+ caches.
   </div>
-  <div id="caption-74" class="caption">
-    The '-config -setpreds ...' allows filtering out packages: those which don't match the given predicate will not be installed. It is a safety option to avoid mixing i686 and x86_64 binaries by mistake.
+  <div id="caption-54" class="caption">
+    The 32b variant has been installed. The script then installs the 64b variant.<br>
+    Meanwhile we can look at a few things that have been installed.
   </div>
-  <div id="caption-101" class="caption">
-    Our last command starts the downloads and installation.
+  <div id="caption-65" class="caption">
+    Demo application of the <a href="http://gtk.org">GTK+</a> graphical toolkit (with a few rough corners on Windows currently).
   </div>
-  <div id="caption-107" class="caption">
-    Packages are downloaded into 'var/cache/packages'.
+  <div id="caption-87" class="caption">
+    Configuration application of <a href="http://enlightenment.org">Elementary</a>, another graphical toolkit.
   </div>
-  <div id="caption-116" class="caption">
-    There are around 60MB of data in this package series; it usually take one or two minutes.
+  <div id="caption-117" class="caption">
+    Demo application of <a href="http://enlightenment.org">Elementary</a>, another graphical toolkit.
   </div>
-  <div id="caption-135" class="caption">
-    Some packages like GCC are quite bigger than the average.
-  </div>
-  <div id="caption-140" class="caption">
-    Playback speed is increased through JS during the package download.
-  </div>
-  <div id="caption-203" class="caption">
-    Once everything is downloaded, installation starts. Packages install very quickly.
-  </div>
-  <div id="caption-210" class="caption">
-    ENOENT errors above are harmless: many packages use POSIX symlinks; for few of these symlinks it's not yet possible to provide them on Windows.
-  </div>
-  <div id="caption-220" class="caption">
-    POSIX symlinks to files are hardlinks and POSIX symlinks to directories are junction points; for our needs they provide a good equivalent to POSIX symlinks.
-  </div>
-  <div id="caption-233" class="caption">
-    Installation finished.
-  </div>
-  <div id="caption-240" class="caption">
-    The 'mingw' directory is an NTFS junction which replaces the original POSIX symlink.
-  </div>
-  <div id="caption-245" class="caption">
-    Browsing around a bit and showing the various documentations, data and files that have been installed.
-  </div>
-  <div id="caption-262" class="caption">
-    And last thing: listing all the packages that are now installed with the 'yypkg -list' command.
+  <div id="caption-128" class="caption">
+    End of the demo. Check the <a href="stable">Documentation/Downloads</a> page for more infos.
   </div>
 </div>
 
