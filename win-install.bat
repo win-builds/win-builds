33a768d522635eae3f43e5de9790151d71ad1797
Adrien Nader <adrien@notk.org>
2014-01-05 22:00:45 +0100

    win-install: make more verbose and don't run gtk-related apps on x64.

--- win-install.bat
+++ win-install.bat
@@ -5,11 +5,17 @@ set OLD_PATH=%PATH%
 set _YY_BITS=32
 set _YY_ARCH=i686
 
+copy *.exe "%YYPREFIX%/bin"
+copy *.dll "%YYPREFIX%/bin"
+
 :do
 set YYPREFIX=C:/win-builds-%_YY_BITS%
 set PATH=%YYPREFIX%/bin;%OLD_PATH%
 
-echo Initializing yypkg in %YYPREFIX%.
+echo ***************************************************
+echo Installing win-builds for %_YY_ARCH% in %YYPREFIX%.
+echo ***************************************************
+
 yypkg -init
 yypkg -config -setpreds host=%_YY_ARCH%-w64-mingw32
 yypkg -config -setpreds target=%_YY_ARCH%-w64-mingw32
@@ -17,14 +23,21 @@ sherpa -set-mirror http://win-builds.org/@@VERSION@@/packages/windows_%_YY_BITS%
 echo Downloading and installing packages.
 sherpa -install all
 
-echo Updating GDK, GTK, Pango and font caches (this may take a while).
-gdk-pixbuf-query-loaders --update-cache
-gtk-query-immodules-2.0 --update-cache
-pango-querymodules --update-cache
+echo Updating fontconfig's cache (takes lot of time and memory on Windows 7/8).
 fc-cache
 
-copy *.exe "%YYPREFIX%/bin"
-copy *.dll "%YYPREFIX%/bin"
+echo Updating pango's module cache.
+REM Pango doesn't respect --libdir for the module cache so simply update the
+REM list in /etc (for now).
+pango-querymodules > %YYPREFIX%/etc/pango/pango.modules
+
+if "%_YY_BITS%"=="64" goto no_gtk
+echo Updating gdk's pixbuf cache.
+gdk-pixbuf-query-loaders --update-cache
+
+echo Updating gtk's immodules cache.
+gtk-query-immodules-2.0 --update-cache
+:no_gtk
 
 if "%_YY_BITS%"=="64" goto done
 
@@ -35,6 +48,6 @@ goto do
 :done
 
 echo Win-builds has been installed.
-echo System settings like PATH haven't been changed; you should now do so.
+echo System settings like PATH haven't been changed; you should do so now.
 
 pause
