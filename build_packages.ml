42e16075c31a688f5855598b9232f13724bb8013
Adrien Nader <adrien@notk.org>
2014-02-02 21:11:16 +0100

    build_package.ml: split part of the build step into a preparation one.
    This helps for two things:
    - it avoids doing preparation twice and saves time (mostly I/Os)
    - it also avoids race conditions where builds for both architectures read
      and access the .tar bundle of a given package at the same time.

--- build_packages.ml
+++ build_packages.ml
@@ -149,12 +149,14 @@ let filter ~kind ~available ~wishes =
     | [] -> available
     | l -> List.filter (fun p -> List.mem p.name l) available
 
-let build ~work_dir ~kind ~available ~wishes ?env () =
-  let packages = filter ~kind ~available ~wishes in
+let prepare ~packages =
   if packages <> [] then (
-    log dbg "Building: %s.\n%!"
+    log dbg "Preparing: %s.\n%!"
       (String.concat ", " (List.map (fun p -> p.name) packages));
     create_process_sync [| "mkdir"; "-p"; work_dir |];
+    let bd_files = [| "build_daemon"; "build_daemon_config" |] in
+    let bd_files = Array.map (Filename.concat source_path) bd_files in
+    cp ~dest:work_dir ~sources:bd_files;
     ListLabels.iter packages ~f:(fun p ->
       create_process_sync [|
         "tar"; "cf"; sp "%s/%s.tar" work_dir p.name;
@@ -163,12 +165,15 @@ let build ~work_dir ~kind ~available ~wishes ?env () =
         "-C"; Filename.concat p.dir p.package; "."
       |]
     );
+  )
+
+let build ~work_dir ~kind ?env packages =
+  if packages <> [] then (
+    log dbg "Building: %s.\n%!"
+      (String.concat ", " (List.map (fun p -> p.name) packages));
     if chroot then
       assert false
     else (
-      let bd_files = [| "build_daemon"; "build_daemon_config" |] in
-      let bd_files = Array.map (Filename.concat source_path) bd_files in
-      cp ~dest:work_dir ~sources:bd_files;
       Some (create_process_async ?env (Array.concat [
         [| Filename.concat work_dir "build_daemon"; kind |];
         Array.map (fun p -> p.name) (Array.of_list packages)
@@ -301,13 +306,23 @@ let () =
    * wrong permissions in the packages, like 711 for /usr, and will break
    * systems. *)
   ignore (Unix.umask 0o022);
-  let f ~kind ~triplet =
-    let env = [| sp "TMP=/tmp/win-builds-%s" triplet |] in
-    let kind, available = kind ~triplet in
-    build ~env ~work_dir ~kind ~available ~wishes ()
-  in
   let kind, available = native_toolchain in
-  may waitpid (build ~work_dir ~kind ~available ~wishes ());
+  let packages = filter ~kind ~available ~wishes in
+  prepare ~packages;
+  may waitpid (build ~work_dir ~kind packages);
   ListLabels.iter [ cross_toolchain; windows ] ~f:(fun kind ->
-    List.iter (may waitpid) (List.map (fun triplet -> f ~kind ~triplet) triplets);
+    let packages = ListLabels.map triplets ~f:(fun triplet ->
+      let kind, available = kind ~triplet in
+      triplet, filter ~kind ~available ~wishes
+    )
+    in
+    let l = List.fold_left (fun l c -> List.rev_append (snd c) l) [] packages in
+    prepare (List.sort_uniq compare l);
+    let pids = ListLabels.map packages ~f:(fun (triplet, packages) ->
+      let env = [| sp "TMP=/tmp/win-builds-%s" triplet |] in
+      let kind, _ = kind ~triplet in
+      build ~env ~work_dir ~kind packages
+    )
+    in
+    List.iter (may waitpid) pids
   )
