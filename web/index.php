c2e52b1463b1fb8e2317e223121c8c2eb8124a99
Adrien Nader <adrien@notk.org>
2014-03-04 18:12:58 +0100

    web: rely on pure text (along with its reflow) for the navigation header.

--- web/index.php
+++ web/index.php
@@ -19,8 +19,8 @@
   <div class="hl">
     <h2 class="hl-title">For 32bits and 64bits</h2>
     <ul class="hl-list">
-      <li class="hl-el">GCC 4.8.2 - C, C++</li>
-      <li class="hl-el">Mingw-w64 3.1.0</li>
+      <li>GCC 4.8.2 - C, C++</li>
+      <li>Mingw-w64 3.1.0</li>
     </ul>
     <div class="hl-text">
     </div>
@@ -28,28 +28,28 @@
   <div class="hl">
     <h2 class="hl-title">From Windows or Linux</h2>
     <ul class="hl-list">
-      <li class="hl-el">Integrate with MSYS, MSYS2, Cygwin or use in native Windows</li>
-      <li class="hl-el">Same packages run in a chroot on any Linux distribution</li>
+      <li>Integrate with MSYS, MSYS2, Cygwin or use in native Windows</li>
+      <li>Same packages run in a chroot on any Linux distribution</li>
     </ul>
   </div>
   <div class="hl">
     <h2 class="hl-title">Large, coherent set of packages</h2>
     <ul class="hl-list">
-      <li class="hl-el">Curl, EFL, freetype, GTK+, lua, openssl, sqlite, wget, zlib</li>
-      <li class="hl-el"><a href="http://win-builds.org/stable/packages/windows_32/package_list.html">More than 55 libraries and tools on Windows</a></li>
+      <li>Curl, EFL, freetype, GTK+, lua, openssl, sqlite, wget, zlib</li>
+      <li><a href="http://win-builds.org/stable/packages/windows_32/package_list.html">More than 55 libraries and tools on Windows</a></li>
     </ul>
   </div>
   <div class="hl">
     <h2 class="hl-title">Quick installs</h2>
     <ul class="hl-list">
-      <li class="hl-el">Set up a complete development environment in 5 minutes on a broadband connection</li>
+      <li>Set up a complete development environment in 5 minutes on a broadband connection</li>
     </ul>
   </div>
   <div class="hl">
     <h2 class="hl-title">Fully-scripted, clean, reproducible builds</h2>
     <ul class="hl-list">
-      <li class="hl-el">Be in control of your infrastructure</li>
-      <li class="hl-el">Customize as you wish</li>
+      <li>Be in control of your infrastructure</li>
+      <li>Customize as you wish</li>
     </ul>
   </div>
 </div>
