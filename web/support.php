ce76e1270c3b8169dfdf505db685928264f3ec59
Adrien Nader <adrien@notk.org>
2014-03-15 09:40:36 +0100

    web: mention mailing-lists on the support page.

--- web/support.php
+++ web/support.php
@@ -16,8 +16,25 @@ For most issues, you can use the <a href="bugs">bug tracker</a>.
 You can also join the #mingw-w64 channel on OFTC (or on freenode where messages 
 will be relayed to OFTC).
 
-<h2>Mailing-list</h2>
-If you prefer, you can also send an email to the <a href="https://lists.sourceforge.net/lists/listinfo/mingw-w64-public">mingw-w64 mailing-list</a>.
+<h2>Mailing-lists</h2>
+<ul>
+<?php
+function print_list($name, $description) {
+  printf('<li>');
+  printf('%s: <code>%s AT lists.win-builds.org</code><br>', $description, $name);
+  printf('Subscribe by sending an email titled <code>subscribe</code> to <code>%s-request AT lists.win-builds.org</code>. Unscribe instructions are similar.<br>',
+    $name);
+  printf('<a href="/lists/%s">Archives can be browsed freely</a>.', $name);
+  printf('</li>');
+}
+?>
+<?php
+print_list('users', 'For all questions related to win-builds');
+print_list('security', 'To receive notifications about security updates');
+print_list('announce', 'To receive notifications about new versions');
+?>
+  <li>The <a href="https://lists.sourceforge.net/lists/listinfo/mingw-w64-public">mingw-w64 mailing-list</a> can also be appropriate.
+</ul>
 
 <h2>Mail</h2>
 If really none of the above fits your situation, you can send an e-mail to adrien AT notk.org.
