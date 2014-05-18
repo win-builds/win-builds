953e1072eb2f1e93f221b795600b350f98c1d469
Adrien Nader <adrien@notk.org>
2014-05-18 18:12:37 +0200

    doc, web: small updates.

--- web/download.php
+++ web/download.php
@@ -10,28 +10,23 @@
 <h1>Windows</h1>
 
 Installation on Windows can be done for MSYS, Cygwin or without them.
-Download the <a href="@@VERSION@@/yypkg-@@VERSION@@.exe">package manager</a> (2MB) and:
+Download the <a href="@@VERSION@@/yypkg-@@VERSION@@.exe">package manager</a> (570KB) and:
 <ul>
   <li>for use without MSYS or Cygwin: double-click on it</li>
-  <li>for MSYS: run it from the command-line as <pre style="display: inline">yypkg-@@VERSION@@.exe --deploy --host msys</pre> and read <a href="@@VERSION@@/msys-cygwin.html#_win_builds_installation">how to switch to the toolchain you want</a>.</li>
-  <li>for Cygwin: run it from the command-line as <pre style="display: inline">yypkg-@@VERSION@@.exe --deploy --host cygwin</pre> and read <a href="@@VERSION@@/msys-cygwin.html#_win_builds_installation">how to switch to the toolchain you want</a>.</li>
+  <li>for MSYS: run it from the command-line as: <pre style="display: inline">yypkg-@@VERSION@@.exe --deploy --host msys</pre> and read <a href="@@VERSION@@/msys-cygwin.html#_change_toolchain_on_the_fly">how to switch to the toolchain you want</a>.</li>
+  <li>for Cygwin: run it from the command-line as: <pre style="display: inline">yypkg-@@VERSION@@.exe --deploy --host cygwin</pre> and read <a href="@@VERSION@@/msys-cygwin.html#_change_toolchain_on_the_fly">how to switch to the toolchain you want</a>.</li>
 </ul>
 
 <p>
-It downloads and manages the complete set of packages (around 65MB compressed, 
-400MB uncompressed).
+Packages are 65MB and size on disk is 400MB. Note that installation doesn't
+change system settings and in particular doesn't change environment variables
+like PATH or PKG_CONFIG_PATH/LIBDIR.
 </p>
 
 <p>
-More information can be found in the <a href="@@VERSION@@">documentation</a>; 
-make sure to subscribe at least to the <a href="support.html">(security) 
+More information can be found in the <a href="@@VERSION@@">documentation</a>;
+make sure to subscribe at least to the <a href="support.html">(security)
 announces mailing-list</a>.
-</p>
-
-<p>
-If you have neither MSYS nor Cygwin installed but want to install one of them, 
-refer to the <a href="@@VERSION@@/msys-cygwin.html">corresponding 
-documentation</a>.
 </p>
 
 <h1>Linux</h1>
@@ -40,13 +35,16 @@ Installation on Linux builds GCC locally in order to have binaries which match t
 </p>
 
 <p>
-The detailled explanations are in the <a 
+The detailled explanations are in the <a
 href="@@VERSION@@/linux.html">documentation for Linux</a>.
 </p>
 
 <h1>Other systems</h1>
 <p>
-It shall be possible to use win-builds on other systems too even if this has not been tried so far. The main requirement is a POSIX system with a GNU userspace.
+It shall be possible to use win-builds on other systems too even if this has
+not been tried so far. The main requirement is a POSIX system with a GNU
+userspace and building will follow the steps in the <a
+href="@@VERSION@@/linux.html">documentation for Linux</a>.
 </p>
 
 <?php include 'footer.html'; ?>
