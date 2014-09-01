df80a5891c391c037e6f38b645f7bd3b3356165c
Adrien Nader <adrien@notk.org>
2014-09-01 18:53:35 +0200

    web: reword and reorder feature highlights on main page.

--- web/index.php
+++ web/index.php
@@ -20,30 +20,32 @@
 
 <div class="flexbox">
   <div class="hl">
-    <h2 class="hl-title">32/64bits</h2>
+    <h2 class="hl-title">From any OS</h2>
     <ul class="hl-list">
-      <li>GCC 4.8.2 - C, C++</li>
-      <li>Mingw-w64 3.1.0</li>
-    </ul>
-  </div>
-  <div class="hl">
-    <h2 class="hl-title">Windows, Linux</h2>
-    <ul class="hl-list">
-      <li>Use in MSYS, Cygwin, bare Windows</li>
+      <li>Run from MSYS, Cygwin or bare Windows</li>
       <li>On Linux, build only the cross-compiler, reuse all other packages</li>
+      <li>Or use the virtual machines and containers</li>
     </ul>
   </div>
   <div class="hl">
     <h2 class="hl-title">Large, coherent set of packages</h2>
     <ul class="hl-list">
-      <li>Curl, EFL, freetype, GTK+, lua, openssl, sqlite, wget, zlib, ...</li>
-      <li><a href="@@VERSION_STABLE@@/packages/windows_32/package_list.html">More than 60 libraries and tools on Windows</a></li>
+      <li>Qt, Curl, EFL, freetype, GTK+, lua, openssl, sqlite, wget, zlib, ...</li>
+      <li><a href="@@VERSION_STABLE@@/packages/windows_32/package_list.html">90 libraries and tools on Windows</a></li>
     </ul>
   </div>
   <div class="hl">
-    <h2 class="hl-title">Quick installs</h2>
+    <h2 class="hl-title">Quick and simple installs</h2>
     <ul class="hl-list">
       <li>Set up a complete development environment in 5 minutes on a broadband connection</li>
+      <li>Test without any setup thanks to prepared virtual machines</li>
+    </ul>
+  </div>
+  <div class="hl">
+    <h2 class="hl-title">32/64bits</h2>
+    <ul class="hl-list">
+      <li>GCC 4.8.3 - C, C++</li>
+      <li>Mingw-w64 3.2.0</li>
     </ul>
   </div>
   <div class="hl">
@@ -51,6 +53,7 @@
     <ul class="hl-list">
       <li>Be in control of your infrastructure</li>
       <li>Customize as you wish</li>
+      <li>Fully-documented</li>
     </ul>
   </div>
   <div class="hl">
