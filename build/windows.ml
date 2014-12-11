a61f50256f5799defc43d814b801b8a23cdcec08
Adrien Nader <adrien@notk.org>
2014-12-11 22:25:28 +0100

    Build/Windows: move to cairo 1.12.18 to fix several non-security issues.
    Details at http://cairographics.org/news/cairo-1.12.18/ .

--- build/windows.ml
+++ build/windows.ml
@@ -389,10 +389,10 @@ let do_adds builder =
       ~dir:"slackware64-current/l"
       (* TODO: seems to be able to use pthread *)
       ~dependencies:[ pixman; freetype; fontconfig; libpng ]
-      ~version:"1.12.16"
+      ~version:"1.12.18"
       ~build:1
       ~sources:[
-        "${PACKAGE}-${VERSION}.tar.xz", "fce23fb9f2af3fe18c0214551e68712d401f6ef4";
+        "${PACKAGE}-${VERSION}.tar.xz", "a76940b58da9c83b8934264617135326c0918f9d";
       ]
     in
 
